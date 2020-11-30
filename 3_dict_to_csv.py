d246b0bec7f1a9a45483931de7bdbd6ce65df5b5
Alena Konovalova <konovalova.alena@gmail.com>
2020-11-30 21:00:24 +0300

    solve dict_to_csv

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,21 +1,32 @@
+import csv
+
 """
 
 Домашнее задание №2
 
 Работа csv
 
-1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору. 
+1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору.
    В списке нужно создать не менее 4-х словарей
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+EMPLOYEES = [
+    {'name': 'Bob', 'age': 24, 'job': 'driver'},
+    {'name': 'Eugenia', 'age': 33, 'job': 'manager'},
+    {'name': 'Nick', 'age': 18, 'job': 'sales assistant'},
+    {'name': 'Lora', 'age': 29, 'job': 'accountant'}
+]
+
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('employees.csv', 'w', newline='') as employees_file:
+        fields = list(EMPLOYEES[0].keys())
+        writer = csv.DictWriter(employees_file, fields, delimiter=';')
+        writer.writeheader()
+        for employee in EMPLOYEES:
+            writer.writerow(employee)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
